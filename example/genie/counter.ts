d5e5a33cb487624dcb5116631ca192ac476bdf1c
shiyt <shiyingtian_thu@163.com>
2023-07-06 18:41:49 -0700

    Set local version of DSL & Fix some bugs

--- example/genie/counter.ts
+++ example/genie/counter.ts
@@ -1,81 +1,103 @@
-import {GenieKey, sharedState} from "reactgenie-lib";
-import {GenieClass, GenieFunction, GenieProperty, DataClass, float, int} from "reactgenie-lib"
+import { GenieKey, HelperClass, sharedState } from "reactgenie-lib";
+import {
+  GenieClass,
+  GenieFunction,
+  GenieProperty,
+  DataClass,
+  float,
+  int,
+} from "reactgenie-lib";
 import "reflect-metadata";
 
 @GenieClass("A counter")
 export class Counter extends DataClass {
-    @GenieKey
-    @GenieProperty()
-    public name: string;
-    @GenieProperty()
-    public type: string;
-    @GenieProperty()
-    public count: int;
+  @GenieKey
+  @GenieProperty()
+  public name: string;
+  @GenieProperty()
+  public type: string;
+  @GenieProperty()
+  public count: int;
 
-    constructor({name, counterType}: {name: string, counterType: string}) {
-        super({});
-        this.count = 0;
-        this.name = name.toLowerCase();
-        this.type = counterType.toLowerCase();
-    }
 
-    @GenieProperty()
-    static Version: float = 1.0;
+  constructor({
+    name,
+    counterType,
+  }: {
+    name: string;
+    counterType: string;
+  }) {
+    super({ name: name });
+    this.count = 0;
+    this.name = name.toLowerCase();
+    this.type = counterType.toLowerCase();
 
-    static setup() {
-        Counter.CreateObject({name: "coin", counterType: "coin"})
-        Counter.CreateObject({name: "tomato", counterType: "vegetable"})
-        Counter.CreateObject({name: "potato", counterType: "vegetable"})
-        Counter.CreateObject({name: "apple", counterType: "fruit"})
-        Counter.CreateObject({name: "banana", counterType: "fruit"})
-        Counter.CreateObject({name: "orange", counterType: "fruit"})
-        Counter.CreateObject({name: "grape", counterType: "fruit"})
-        Counter.CreateObject({name: "mango", counterType: "fruit"})
-        Counter.CreateObject({name: "pineapple", counterType: "fruit"})
-        Counter.CreateObject({name: "strawberry", counterType: "fruit"})
-        Counter.CreateObject({name: "watermelon", counterType: "fruit"})
-    }
-    @GenieFunction()
-    static GetCounter({name = ""}: {name?: string}): Counter {
-        return Counter.GetObject({name: name});
-    }
+ 
+  }
 
-    @GenieFunction()
-    increment(): void {
-        this.count = this.count + 1;
-    }
+  @GenieProperty()
+  static Version: float = 1.0;
 
-    @GenieFunction()
-    decrement(): void {
-        this.count -= 1;
-    }
+  static setup() {
+    Counter.CreateObject({
+      name: "coin",
+      counterType: "coin"
+    });
+    Counter.CreateObject({
+      name: "tomato",
+      counterType: "vegetable"
+    });
+    Counter.CreateObject({ name: "potato", counterType: "vegetable" });
+    Counter.CreateObject({ name: "apple", counterType: "fruit" });
+    Counter.CreateObject({ name: "banana", counterType: "fruit" });
+    Counter.CreateObject({ name: "orange", counterType: "fruit" });
+    Counter.CreateObject({ name: "grape", counterType: "fruit" });
+    Counter.CreateObject({ name: "mango", counterType: "fruit" });
+    Counter.CreateObject({ name: "pineapple", counterType: "fruit" });
+    Counter.CreateObject({ name: "strawberry", counterType: "fruit" });
+    Counter.CreateObject({ name: "watermelon", counterType: "fruit" });
+  }
+  @GenieFunction()
+  static GetCounter({name = ""}: {name?: string}): Counter {
+      return Counter.GetObject({name: name});
+  }
 
-    description(): {} {
-        return {
-            "count": this.count
-        }
-    }
+  @GenieFunction()
+  increment(): void {
+    this.count = this.count + 1;
+  }
+
+  @GenieFunction()
+  decrement(): void {
+    this.count -= 1;
+  }
+
+  description(): {} {
+    return {
+      count: this.count,
+    };
+  }
 }
 
 export const CounterExamples = [
-    {
-        user_utterance: "increment",
-        example_parsed: "Counter.Current().increment()",
-    },
-    {
-        user_utterance: "what is the count",
-        example_parsed: "Counter.Current().count",
-    },
-    {
-        user_utterance: "what is the count of potato",
-        example_parsed: 'Counter.GetCounter(name: "potato").count',
-    },
-    {
-        user_utterance: "increment potato counter",
-        example_parsed: 'Counter.GetCounter(name: "potato").increment()',
-    },
-    {
-        user_utterance: "show me all vegetables counters",
-        example_parsed: 'Counter.All().matching(field: .type, value: "vegetable")',
-    }
-]
+  {
+    user_utterance: "increment",
+    example_parsed: "Counter.Current().increment()",
+  },
+  {
+    user_utterance: "what is the count",
+    example_parsed: "Counter.Current().count",
+  },
+  {
+    user_utterance: "what is the count of potato",
+    example_parsed: 'Counter.GetCounter(name: "potato").count',
+  },
+  {
+    user_utterance: "increment potato counter",
+    example_parsed: 'Counter.GetCounter(name: "potato").increment()',
+  },
+  {
+    user_utterance: "show me all vegetables counters",
+    example_parsed: 'Counter.All().matching(field: .type, value: "vegetable")',
+  },
+];
