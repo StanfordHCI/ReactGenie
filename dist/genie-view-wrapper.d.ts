cd16a0c0ed80349c3bf6288eb3680e849093f9eb
Jackie Yang <jackie@jackieyang.me>
2023-09-02 09:59:22 -0700

    Remove dist

--- dist/genie-view-wrapper.d.ts
+++
@@ -1,2 +0,0 @@
-import React from "react";
-export declare const GenieViewWrapper: (className: string, childComponent: any, childParams: any[]) => () => React.JSX.Element;
--- dist/speech-recognition.d.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export declare const SpeechRecognizer: (props: {
-    shouldListen: boolean;
-    speechStatusCallback: (status: boolean, transcript: string) => void;
-    speechResultCallback: (result: string) => void;
-    azureSpeechRegion: string;
-    azureSpeechKey: string;
-}) => React.JSX.Element;
